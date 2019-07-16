bb71dd98b5c3e54f461bc05fb1a4882a69880388
ashtashess <ashtashess@users.noreply.github.com>
2019-07-15 23:13:03 -0400

    Completed Tier 1

--- TileMap Tutorial/Assets/Scripts/PlayerController.cs
+++ TileMap Tutorial/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rb2d;
+    private int count;
     public float speed;
     public float jumpForce;
+    public Text countText;
+    public Text winText;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetAllText();
     }
 
-    // Update is called once per frame
-    void Update() {
+    private void SetAllText()
+    {
+        throw new NotImplementedException();
     }
-    
-        void FixedUpdate()
+
+    void FixedUpdate()
         {
 
 
@@ -33,6 +42,25 @@ public class PlayerController : MonoBehaviour
             Vector2 movement = new Vector2(moveHozizontal, 0);
 
             rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetAllText();
+        }
+
+        void SetAllText()
+        {
+            countText.text = "Count " + count.ToString();
+            if (count>= 4)
+            {
+                winText.text = "You Win!";
+            }
+        }
     }
 
     void OnCollisionStay2D(Collision2D collision)
